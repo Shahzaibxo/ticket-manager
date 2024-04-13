0e233b467bb7e98a00fd7cdcdea5564c6ec78725
Shahzaibxo <shahziwork@gmail.com>
2024-04-13 13:10:38 +0500

    New-issue component

---
+++ app/issues/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+       <Link href="/issues/new">
+        <Button>okok</Button>
+       </Link>
+    </div>
+  )
+}
+
+export default page
